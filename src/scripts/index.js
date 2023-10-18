eaab4b2c247778314c31dd64e02ce3c58fb48d9a
Victor Carvalho de Jesus <cictorvarvalho@gmail.com>
2023-10-18 16:50:32 -0300

    att 2

---
+++ src/scripts/index.js
@@ -0,0 +1,20 @@
+function handleModal(){
+    const button = document.querySelector('.header_button')
+    const modalContainer = document.querySelector('#modalController')
+
+    button .addEventListener('click', () => {
+        modalContainer.showModal()
+
+        closeModal()
+    })
+}
+
+function closeModal(){
+    const button = document.querySelector('#closeModal')
+    const modalContainer = document.querySelector('#modalController')
+
+    button.addEventListener('click', () => {
+        modalContainer.close()
+    })
+}
+handleModal()
