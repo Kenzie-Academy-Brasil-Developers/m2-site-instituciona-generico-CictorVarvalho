77b493c1fedc2d94ccac3b4759e8ae7ffcfade04
Victor Carvalho de Jesus <cictorvarvalho@gmail.com>
2023-10-20 12:36:07 -0300

    5 final

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,25 @@
-function handleModal(){
-    const button = document.querySelector('.header_button')
-    const modalContainer = document.querySelector('#modalController')
+function handleModal () {
+    const buttonHeader = document.querySelector(".header__button");
+    const buttonFaq = document.querySelector(".faq__button");
+    const modalController = document.querySelector(".modal__controller");
 
-    button .addEventListener('click', () => {
-        modalContainer.showModal()
+    buttonHeader.addEventListener("click", function () {
+        modalController.showModal();
+    })
 
-        closeModal()
+    buttonFaq.addEventListener("click", function () {
+        modalController.showModal();
     })
 }
 
-function closeModal(){
-    const button = document.querySelector('#closeModal')
-    const modalContainer = document.querySelector('#modalController')
-
-    button.addEventListener('click', () => {
-        modalContainer.close()
+function closeModal () {
+    const buttonClose = document.querySelector(".modal__button--close");
+    const modalController = document.querySelector(".modal__controller");
+    
+    buttonClose.addEventListener("click", function () {
+        modalController.close();
     })
 }
-handleModal()
+
+handleModal();
+closeModal();
